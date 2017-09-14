d5ca12b099938497c0aa38087f8301f45802188d
JaviOrtiz <javito_97.prat@hotmail.com>
2017-09-14 20:02:15 +0200

    ImGui files Added, we need to make the help window

--- Game-Engine/ModuleRenderer3D.cpp
+++ Game-Engine/ModuleRenderer3D.cpp
@@ -4,6 +4,9 @@
 #include "SDL\include\SDL_opengl.h"
 #include <gl/GL.h>
 #include <gl/GLU.h>
+#include "ImGui\imgui.h"
+#include "ImGui/imgui_impl_sdl_gl3.h"
+
 
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
